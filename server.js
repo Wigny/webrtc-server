6b2eabf6cfc8435a897ed0884d726c451f1cabb6
Wígny Almeida <wignybora@gmail.com>
2020-05-10 19:12:49 -0300

    Add socket

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(4000, () => console.log('Server is running'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.send({
+  runnig: true,
+}));
+
+io.on('error', console.error);
+
+let broadcaster;
+
+io.on('connection', socket => {
+  socket.on('broadcaster', () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit('broadcaster');
+  });
+
+  socket.on('watcher', () => socket
+    .to(broadcaster)
+    .emit('watcher', socket.id)
+  );
+
+  socket.on('disconnect', () => socket
+    .to(broadcaster)
+    .emit('disconnectPeer', socket.id)
+  );
+
+  socket.on('offer', (id, message) => socket
+    .to(id)
+    .emit('offer', socket.id, message)
+  );
+
+  socket.on('answer', (id, message) => socket
+    .to(id)
+    .emit('answer', socket.id, message)
+  );
+
+  socket.on('candidate', (id, message) => socket
+    .to(id)
+    .emit('candidate', socket.id, message));
+});
