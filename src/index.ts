ba90203681491ba5175fd71f74947b352dccdb92
Wígny Almeida <wigny.almeida@credisis.com.br>
2021-01-13 10:39:26 -0300

    Simplifica servidor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import { App, PORT } from './app';
+import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
-const { server } = new App();
+const app = express();
+const http = createServer(app);
+const io = new Server(http, {
+  cors: { origin: '*' },
+});
 
-server.listen(process.env.PORT, () =>
-  console.log(`Running in ${PORT}`)
-);
+let broadcaster: string;
+
+io.on('connection', (socket: Socket) => {
+  socket.on('broadcaster', () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit('broadcaster');
+  });
+
+  socket.on('watcher', () => socket
+    .to(broadcaster)
+    .emit('watcher', socket.id),
+  );
+
+  socket.on('disconnect', () => socket
+    .to(broadcaster)
+    .emit('disconnectPeer', socket.id),
+  );
+
+  socket.on('offer', (id, message) => socket
+    .to(id)
+    .emit('offer', socket.id, message),
+  );
+
+  socket.on('answer', (id, message) => socket
+    .to(id)
+    .emit('answer', socket.id, message),
+  );
+
+  socket.on('candidate', (id, message) => socket
+    .to(id)
+    .emit('candidate', socket.id, message));
+});
+
+http.listen(process.env.PORT || 8080, () => {
+  console.log(`Running in ${process.env.PORT || 8080}`);
+});
